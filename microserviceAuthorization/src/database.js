c223cbc1ce9c532a6e01bed9e737f108e224b3a1
JFabianMM <103282271+JFabianMM@users.noreply.github.com>
2023-06-20 08:06:05 -0700

    Add files via upload

--- microserviceAuthorization/src/database.js
+++ microserviceAuthorization/src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI)
+//mongoose.connect('mongodb://fabian:12345@mongodb3:27017/mydatabase?authSource=admin')
+mongoose.connect(process.env.MONGODB3_URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
 
